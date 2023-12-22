e5dc134ad999b4857f6efb631792ace9127665fb
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2023-12-22 11:48:50 -0500

    folder changes

--- thefactory-frontend/src/AppRouter.tsx
+++ thefactory-frontend/src/AppRouter.tsx
@@ -1,8 +1,8 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ResourcesPage from "./pages/ResourcesPage.tsx";
-import WorkshopsPage from "./pages/WorkshopsPage.tsx";
-import OfficeHoursPage from "./pages/OfficeHoursPage.tsx";
-import HomePage from "./pages/HomePage.tsx";
+import ResourcesPage from "./pages/ResourcesPage/ResourcesPage.tsx";
+import WorkshopsPage from "./pages/WorkshopsPage/WorkshopsPage.tsx";
+import OfficeHoursPage from "./pages/OfficeHoursPage/OfficeHoursPage.tsx";
+import HomePage from "./pages/HomePage/HomePage.tsx";
 
 function AppRouter() {
     return (
